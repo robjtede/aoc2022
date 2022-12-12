45ea8e5efbc8fe0fc688d9f23ac67d9b0d8d1e77
Rob Ede <robjtede@icloud.com>
2022-12-12 23:26:05 +0000

    day 6

---
+++ src/day6.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+
+static INPUT: &str = include_str!(concat!("./", module_path!(), "_input.txt"));
+static INPUT_TEST: &str = include_str!(concat!("./", module_path!(), "_test.txt"));
+
+fn distinct_n_until(line: &str, n: usize) -> usize {
+    line.as_bytes()
+        .windows(n)
+        .take_while(|&slice| HashSet::<&u8>::from_iter(slice).len() != n)
+        .count()
+        + n
+}
+
+fn main() {
+    let input = match std::env::args().skip(1).next() {
+        Some(flag) if flag == "--test" => INPUT_TEST,
+        _ => INPUT,
+    };
+
+    for line in input.lines() {
+        // problem A
+
+        let solution_a = distinct_n_until(line, 4);
+        println!("solution A = {solution_a}");
+
+        // problem B
+
+        let solution_b = distinct_n_until(line, 14);
+        println!("solution B = {solution_b}");
+
+        println!();
+    }
+}
