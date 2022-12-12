a104c41ef77ded3ed74e71b3c7c76bf6dd06a972
Rob Ede <robjtede@icloud.com>
2022-12-12 23:06:53 +0000

    day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,21 +1,98 @@
+use std::collections::VecDeque;
+
 static INPUT: &str = include_str!(concat!("./", module_path!(), "_input.txt"));
 static INPUT_TEST: &str = include_str!(concat!("./", module_path!(), "_test.txt"));
 
-fn main() {
-    let test = std::env::args()
-        .skip(1)
-        .next()
-        .map_or(false, |flag| flag == "--test");
+fn parse_layout(layout: &str) -> Vec<Vec<char>> {
+    let mut stacks = vec![VecDeque::with_capacity(8); 9];
 
-    let input = if test { INPUT_TEST } else { INPUT };
+    for stack in layout.lines() {
+        for (n, boite) in stack.chars().enumerate() {
+            if boite != ' ' {
+                stacks[n].push_front(boite);
+            }
+        }
+    }
+
+    stacks.into_iter().map(Vec::from).collect()
+}
+
+fn parse_movement(line: &str) -> (usize, usize, usize) {
+    let mut split = line.splitn(3, ' ');
+    let num = split.next().unwrap().parse::<usize>().unwrap();
+    let from = split.next().unwrap().parse::<usize>().unwrap() - 1;
+    let to = split.next().unwrap().parse::<usize>().unwrap() - 1;
+    (num, from, to)
+}
+
+fn main() {
+    let input = match std::env::args().skip(1).next() {
+        Some(flag) if flag == "--test" => INPUT_TEST,
+        _ => INPUT,
+    };
+
+    let (layout, moves) = input.split_once("\n\n").unwrap();
 
     // part A
+    {
+        let mut stacks = parse_layout(layout);
 
-    let solution_a = "TODO";
-    println!("solution A = {solution_a}");
+        print_stacks(&stacks);
+
+        for (num, from, to) in moves.lines().map(parse_movement) {
+            println!("move {num} from {from} to {to}");
+
+            let len = stacks[from].len();
+            let a = stacks[from].drain(len - num..).rev().collect::<Vec<_>>();
+            stacks[to].extend(a);
+
+            println!();
+            print_stacks(&stacks);
+        }
+
+        let solution_a = stacks
+            .iter()
+            .filter_map(|stack| stack.last())
+            .collect::<String>();
+        println!("solution A = {solution_a}");
+    }
 
     // part B
+    {
+        let mut stacks = parse_layout(layout);
 
-    let solution_b = "TODO";
-    println!("solution B = {solution_b}");
+        print_stacks(&stacks);
+
+        for (num, from, to) in moves.lines().map(parse_movement) {
+            println!("move {num} from {from} to {to}");
+
+            let len = stacks[from].len();
+            let a = stacks[from].drain(len - num..).collect::<Vec<_>>();
+            stacks[to].extend(a);
+
+            println!();
+            print_stacks(&stacks);
+        }
+
+        let solution_b = stacks
+            .iter()
+            .filter_map(|stack| stack.last())
+            .collect::<String>();
+
+        println!("solution B = {solution_b}");
+    }
+}
+
+// visualization helpers
+
+fn print_stacks(stacks: &Vec<Vec<char>>) {
+    for (n, stack) in stacks.iter().enumerate() {
+        let mut line = String::new();
+        line.push_str(&format!("{n} "));
+        for &boite in stack {
+            line.push(boite);
+        }
+        println!("{line}");
+    }
+    println!()
 }
