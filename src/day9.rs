fea1546dbbd97abd115316ad73dab04db95e860e
Rob Ede <robjtede@icloud.com>
2022-12-12 20:08:51 +0000

    day 9b

--- src/day9.rs
+++ src/day9.rs
@@ -1,88 +1,152 @@
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 
 static INPUT: &str = include_str!(concat!("./", module_path!(), "_input.txt"));
 static INPUT_TEST: &str = include_str!(concat!("./", module_path!(), "_test.txt"));
+static INPUT_TEST_LARGE: &str = include_str!(concat!("./", module_path!(), "_test_large.txt"));
 
-fn dir_move(dir: &str, pos: &mut (i64, i64)) {
-    match dir {
-        "R" => pos.0 += 1,
-        "U" => pos.1 += 1,
-        "L" => pos.0 -= 1,
-        "D" => pos.1 -= 1,
-        _ => panic!("invalid dir {dir}"),
-    }
+/// Returns mutable reference to both `i` and `i+1` positions in `arr`.
+fn pair_mut(arr: &mut [(i64, i64)], i: usize) -> (&mut (i64, i64), &mut (i64, i64)) {
+    let (h, t) = arr[i..=(i + 1)].split_at_mut(1);
+    (&mut h[0], &mut t[0])
 }
 
+/// Returns pythagorean distance between `h` and `t`.
 fn distance((hx, hy): (i64, i64), (tx, ty): (i64, i64)) -> f64 {
     (((hx - tx) as f64).abs().powi(2) + ((hy - ty) as f64).abs().powi(2)).sqrt()
 }
 
-#[derive(Debug)]
-enum MoveType {
-    None,
-    Linear,
-    Diag,
+/// Returns 1 if `b` is true, otherwise returns 2.
+fn bool_to_int(b: bool) -> i64 {
+    b as u8 as i64 * 2 - 1
 }
 
-fn move_type(h: (i64, i64), t: (i64, i64)) -> MoveType {
-    let dist = distance(h, t);
-
-    // values chosen between square roots of 2, 3, and 4
-
-    if dist > 2.01 {
-        MoveType::Diag
-    } else if dist > 1.5 {
-        MoveType::Linear
-    } else {
-        MoveType::None
+/// Moves `h` in direction `dir`.
+fn move_h(dir: &str, (hx, hy): &mut (i64, i64)) {
+    match dir {
+        "R" => *hx += 1,
+        "U" => *hy += 1,
+        "L" => *hx -= 1,
+        "D" => *hy -= 1,
+        _ => panic!("invalid direction {dir}"),
     }
 }
 
-fn main() {
-    let test = std::env::args()
-        .skip(1)
-        .next()
-        .map_or(false, |flag| flag == "--test");
+/// Moves `t` to follow `h` assuming that `t` actually needs to move.
+fn move_t((hx, hy): (i64, i64), (tx, ty): &mut (i64, i64)) {
+    let dx = hx - *tx;
+    let dy = hy - *ty;
 
-    let input = if test { INPUT_TEST } else { INPUT };
+    if dx == 0 {
+        // vertical move
+        *ty += dy.signum();
+    } else if dy == 0 {
+        // horizontal move
+        *tx += dx.signum();
+    } else {
+        // diagonal move
+        *tx += bool_to_int(hx > *tx);
+        *ty += bool_to_int(hy > *ty);
+    }
+}
 
-    // problem A
+/// Parses input line into direction and distance.
+fn parse_line(line: &str) -> (&str, i64) {
+    let (dir, dist) = line.split_once(' ').unwrap();
+    (dir, dist.parse::<i64>().unwrap())
+}
 
-    let mut h = (0_i64, 0_i64);
-    let mut t = (0_i64, 0_i64);
+/// Simulate a `LEN` segment rope moving according to `input` instructions, returning a list of the
+/// tail's positions after each instruction.
+fn follow_rope<const LEN: usize>(input: &str) -> HashSet<(i64, i64)> {
+    let mut rope = [(0_i64, 0_i64); LEN];
+    let mut t_locations = HashSet::with_capacity(4096);
 
-    let mut t_set = Vec::<(i64, i64)>::new();
-
+    // each line is an instruction of how H should move
     for line in input.lines() {
-        let (dir, dist) = line.split_once(' ').unwrap();
-        let dist = dist.parse::<i64>().unwrap();
+        let (dir, dist) = parse_line(line);
 
+        // move H in direction {dir} by {dist} steps
         for _ in 0..dist {
-            let h_prev = h;
-
             // move H according to the input
-            dir_move(dir, &mut h);
+            move_h(dir, &mut rope[0]);
 
-            match move_type(h, t) {
-                // H has not moved enough to move T
-                MoveType::None => {}
+            // move each subsequent rope segment (if necessary)
+            for i in 0..(rope.len() - 1) {
+                let (h, t) = pair_mut(&mut rope, i);
 
-                // H has moved linearly away from T, move it the same
-                MoveType::Linear => dir_move(dir, &mut t),
-
-                // H is not in line with T, move T to previous H location
-                MoveType::Diag => t = h_prev,
+                // t needs to move if its pythag distance is larger than sqrt(2)
+                if distance(*h, *t) > 1.5 {
+                    move_t(*h, t);
+                }
             }
 
-            t_set.push(t);
+            // store updated location for rope tail
+            t_locations.insert(*rope.last_mut().unwrap());
         }
     }
 
-    let solution_a = BTreeSet::from_iter(t_set).len();
+    t_locations
+}
+
+fn main() {
+    // let mut print_path = false;
+
+    let input = match std::env::args().skip(1).next() {
+        Some(flag) if flag == "--test" => INPUT_TEST,
+        Some(flag) if flag == "--test-large" => INPUT_TEST_LARGE,
+        // Some(flag) if flag == "--print" => {
+        //     print_path = true;
+        //     INPUT
+        // }
+        _ => INPUT,
+    };
+
+    // problem A
+    let t_locations = follow_rope::<2>(input);
+    // println!("T locations of part A:");
+    // if print_path { print_t_locations(&t_locations); }
+    // let solution_a = BTreeSet::from_iter(t_locations.clone()).len();
+    let solution_a = t_locations.len();
     println!("solution A = {solution_a:?}");
 
-    // problem B
+    println!();
 
-    let solution_b = "TODO";
-    println!("solution B = {solution_b}");
+    // problem B
+    let t_locations = follow_rope::<10>(input);
+    // println!("T locations of part B:");
+    // if print_path { print_t_locations(&t_locations); }
+    // let solution_a = BTreeSet::from_iter(t_locations.clone()).len();
+    let solution_b = t_locations.len();
+    println!("solution B = {solution_b:?}");
+}
+
+// visualisation helpers
+
+fn print_t_locations(t_set: &[(i64, i64)]) {
+    for y in (-20..20).rev() {
+        let mut line = String::new();
+        for x in -40..40 {
+            line.push(if t_set.contains(&(x, y)) { 'X' } else { '.' });
+        }
+        println!("{line}");
+    }
+}
+
+#[allow(dead_code)]
+fn print_board(h: (i64, i64), t: (i64, i64)) {
+    for y in (-8..8).rev() {
+        let mut line = String::new();
+        for x in -8..8 {
+            if h == (x, y) {
+                line.push('H');
+            } else if t == (x, y) {
+                line.push('T');
+            } else {
+                line.push('.');
+            }
+        }
+        println!("{line}");
+    }
+
+    println!();
 }
