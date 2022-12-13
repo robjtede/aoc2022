0fe8bc73f5639883f2e1e20e33ed47afd418ba2a
Rob Ede <robjtede@icloud.com>
2022-12-13 23:50:00 +0000

    day 10a

---
+++ src/day10.rs
@@ -0,0 +1,60 @@
+use std::collections::VecDeque;
+
+static INPUT: &str = include_str!(concat!("./", module_path!(), "_input.txt"));
+static INPUT_TEST: &str = include_str!(concat!("./", module_path!(), "_test.txt"));
+static INPUT_TEST_LARGE: &str = include_str!(concat!("./", module_path!(), "_test_large.txt"));
+
+fn main() {
+    let input = match std::env::args().skip(1).next() {
+        Some(flag) if flag == "--test" => INPUT_TEST,
+        Some(flag) if flag == "--test-large" => INPUT_TEST_LARGE,
+        _ => INPUT,
+    };
+
+    enum State {
+        Noop,
+        AddX(usize, i64),
+    }
+
+    let mut instructions = input
+        .lines()
+        .map(|line| match &line[..4] {
+            "noop" => State::Noop,
+            "addx" => State::AddX(1, line[5..].parse().unwrap()),
+            inst => panic!("invalid instruction: {inst}"),
+        })
+        .collect::<VecDeque<_>>();
+
+    let mut x = 1_i64;
+    let mut cycle = 0_i64;
+    // sum of signal strengths
+    let mut ss_sum = 0;
+
+    loop {
+        let Some(instruction) = instructions.pop_front() else { break; };
+
+        cycle += 1;
+
+        if (cycle - 20) % 40 == 0 {
+            // signal strength
+            let ss = cycle * x;
+            println!("cycle {cycle} signal strength: {ss}");
+            ss_sum += ss;
+        }
+
+        match instruction {
+            State::Noop => {}
+            State::AddX(1, n) => instructions.push_front(State::AddX(0, n)),
+            State::AddX(0, n) => x += n,
+            State::AddX(..) => unreachable!(),
+        }
+    }
+
+    // problem A
+    let solution_a = ss_sum;
+    println!("solution A = {solution_a}");
+
+    // problem B
+    let solution_b = "TODO";
+    println!("solution B = {solution_b}");
+}
