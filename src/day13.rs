68fcc2814290eaaa5e246c529040390a36d514dd
Rob Ede <robjtede@icloud.com>
2022-12-17 13:27:07 +0000

    day 13b

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,4 @@
-use std::cmp;
+use std::{cmp, collections::BTreeSet};
 
 use itertools::{EitherOrBoth, Itertools as _};
 
@@ -8,13 +8,13 @@ static INPUT_TEST: &str = include_str!(concat!("./", module_path!(), "_test.txt"
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Item {
     Single(u64),
-    List(Box<Vec<Item>>),
+    List(Vec<Item>),
 }
 
 impl Item {
     fn to_list(&self) -> Self {
         match self {
-            Item::Single(num) => Self::List(Box::new(vec![Item::Single(*num)])),
+            Item::Single(num) => Self::List(vec![Item::Single(*num)]),
             Item::List(_) => panic!("already a list"),
         }
     }
@@ -22,23 +22,26 @@ impl Item {
 
 impl PartialOrd for Item {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        eprintln!("  comparing {self:?} vs {other:?}");
         match (self, other) {
-            (Self::Single(l), Self::Single(r)) => dbg!(l.partial_cmp(r)),
-            (Self::List(l), Self::List(r)) => {
-                if dbg!(l == r) {
-                    return Some(cmp::Ordering::Equal);
-                }
+            // If both values are integers, the lower integer should come first. If the left integer
+            // is lower than the right integer, the inputs are in the right order. If the left
+            // integer is higher than the right integer, the inputs are not in the right order.
+            // Otherwise, the inputs are the same integer; continue checking the next part of the
+            // input.
+            (Self::Single(l), Self::Single(r)) => l.partial_cmp(r),
 
+            // If both values are lists,...
+            (Self::List(l), Self::List(r)) => {
+                // ... compare the first value of each list, then the second value, and so on.
                 for zip in l.iter().zip_longest(r.iter()) {
-                    match dbg!(zip) {
-                        // If both values are lists, compare the first value of each list, then the
-                        // second value, and so on.
-                        EitherOrBoth::Both(l, r) => dbg!(match l.partial_cmp(r)? {
+                    match zip {
+                        // the task doesn't really say so, but an ordering decision is made as soon
+                        // as one of the comparisons is not equal
+                        EitherOrBoth::Both(l, r) => match l.partial_cmp(r)? {
                             cmp::Ordering::Less => return Some(cmp::Ordering::Less),
                             cmp::Ordering::Equal => {}
                             cmp::Ordering::Greater => return Some(cmp::Ordering::Greater),
-                        }),
+                        },
 
                         // If the right list runs out of items first, the inputs are not in the
                         // right order.
@@ -50,10 +53,10 @@ impl PartialOrd for Item {
                     }
                 }
 
-                Some(cmp::Ordering::Less)
+                Some(cmp::Ordering::Equal)
             }
-            (l @ Self::Single(..), r @ Self::List(..)) => dbg!(l.to_list().partial_cmp(r)),
-            (l @ Self::List(..), r @ Self::Single(..)) => dbg!(l.partial_cmp(&r.to_list())),
+            (l @ Self::Single(..), r @ Self::List(..)) => l.to_list().partial_cmp(r),
+            (l @ Self::List(..), r @ Self::Single(..)) => l.partial_cmp(&r.to_list()),
         }
     }
 }
@@ -81,7 +84,7 @@ fn parse_list(line: &str) -> Vec<Item> {
             ',' if level == 0 => {
                 match part.parse::<u64>() {
                     Ok(num) => items.push(Item::Single(num)),
-                    Err(_) => items.push(Item::List(Box::new(parse_list(&part)))),
+                    Err(_) => items.push(Item::List(parse_list(&part))),
                 };
 
                 part.clear();
@@ -95,10 +98,24 @@ fn parse_list(line: &str) -> Vec<Item> {
 
     match part.parse::<u64>() {
         Ok(num) => items.push(Item::Single(num)),
-        Err(_) => items.push(Item::List(Box::new(parse_list(&part)))),
+        Err(_) => items.push(Item::List(parse_list(&part))),
     };
 
     items
+}
+
+fn is_divider_packet(packet: &[Item], n: u64) -> bool {
+    if packet.len() != 1 {
+        return false;
+    }
+
+    match packet.first() {
+        Some(Item::List(list)) if list.len() == 1 => match list.first() {
+            Some(Item::Single(num)) => *num == n,
+            _ => false,
+        },
+        _ => false,
+    }
 }
 
 fn main() {
@@ -106,6 +123,8 @@ fn main() {
         Some(flag) if flag == "--test" => INPUT_TEST,
         _ => INPUT,
     };
+
+    // problem A
 
     let mut sum = 0;
 
@@ -116,8 +135,6 @@ fn main() {
         .into_iter()
         .map(|iter| iter.collect_tuple().unwrap())
         .enumerate()
-    // .skip(1)
-    // .take(1)
     {
         let i = i + 1;
 
@@ -129,16 +146,40 @@ fn main() {
             cmp::Ordering::Greater => false,
         };
 
-        println!("pair {i} : {ordered:<5} : {a} <= {b}");
+        // println!("pair {i} : {ordered:<5} : {a} <= {b}");
 
         sum += i * ordered as usize;
     }
 
-    // problem A
     let solution_a = sum;
-    println!("solution A = {solution_a}");
 
     // problem B
-    let solution_b = "TODO";
+
+    let packets = input
+        .lines()
+        .chain(["[[2]]", "[[6]]"].into_iter())
+        .filter(|line| !line.is_empty())
+        .map(parse_list)
+        .collect::<BTreeSet<_>>();
+
+    let mut two = 0;
+    let mut six = 0;
+
+    // items are iterated in order for BTreeSet
+    for (i, packet) in packets.into_iter().enumerate() {
+        if is_divider_packet(&packet, 2) {
+            two = i + 1;
+        }
+
+        if is_divider_packet(&packet, 6) {
+            six = i + 1;
+            // break early since we know the [[6]] is after the [[2]]
+            break;
+        }
+    }
+
+    let solution_b = two * six;
+
+    println!("solution A = {solution_a}");
     println!("solution B = {solution_b}");
 }
